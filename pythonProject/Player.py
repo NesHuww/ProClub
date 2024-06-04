15d4bc6e250be457dc97545308be422b8da77c98
Florian Valentin <florian.valentin@etudiant.univ-reims.fr>
2024-06-04 21:08:49 +0200

    Dop Joueur.py OK

--- pythonProject/Player.py
+++ pythonProject/Player.py
@@ -5,6 +5,14 @@ class Player:
     posts = ["GK", "LB", "RB", "CB", "CDM", "CM", "CAM", "LW", "RW", "ST"]
 
     def __init__(self, name: str, height: float|int, weight: float|int, mean: float|int = 6.0, position:str="" ):
+        '''
+        build a player
+        :param name:
+        :param height:
+        :param weight:
+        :param mean optional:
+        :param position optional:
+        '''
         if type(name) != str:
             raise TypeError('name isn\'t a string')
         if type(height) != float and type(height) != int:
@@ -22,46 +30,113 @@ class Player:
         self._mean = mean
 
     def setName(self, name: str) -> None:
+        '''
+        name setter
+        :param name: string
+        :return: none
+        '''
+        if type(name) != str:
+            raise TypeError('name you gave isn\'t string')
         self._name = name
 
     def setPosition(self, position: str) -> None:
+        '''
+        position setter
+        :param position: has to be in available posts (getAvailablePosts)
+        :return: None
+        '''
         if position not in Player.posts:
             raise ValueError('position isn\'t in available positions')
         self._position = position
 
-    def setHeight(self, height) -> None:
+
+    def setHeight(self, height:float|int) -> None:
+        '''
+        height setter
+        :param height: has to be a float or a int
+        :return: None
+        '''
+        if type(height) != float and type(height) != int:
+            raise TypeError('height isn\'t a float or an integer')
         self._height = height
 
     def setWeight(self, weight) -> None:
+        '''
+        weight setter
+        :param weight: has to be a float or a int
+        :return: None
+        '''
+        if type(weight) != float and type(weight) != int:
+            raise TypeError('weight isn\'t a float or an integer')
         self._weight = weight
 
-    def setMean(self, mean: float):
+    def setMean(self, mean: float)->None:
+        '''
+        mean setter
+        :param mean: has to be a float
+        :return: None
+        '''
+        if type(mean) != float:
+            raise TypeError('height isn\'t a float or an integer')
         self._mean = mean
 
     def getName(self) -> str:
+        '''
+        Name getter
+        :return: string
+        '''
         return self._name
 
     def getPosition(self) -> str:
+        '''
+        Position getter
+        :return: string
+        '''
         return self._position
 
-    def getHeight(self) -> float:
+    def getHeight(self) -> float|int:
+        '''
+        Height getter
+        :return: float|int
+        '''
         return self._height
 
-    def getWeight(self) -> float:
+    def getWeight(self) -> float|int:
+        '''
+        Weight getter
+        :return: float|int
+        '''
         return self._weight
 
-    def getMean(self):
+    def getMean(self)->float:
+        '''
+        Mean getter
+        :return: float
+        '''
         return self._mean
+
+    def getAvailablePosts(self):
+        '''
+        Available posts getter
+        :return: array of availables posts
+        '''
+        return Player.posts
+
 
     def hasBetterMean(self, player2: Player) -> bool:
         '''
         Return true if player passed in parameter has a worst mean than actual player.
+        :return: bool
         '''
         if type(player2) != Player:
             raise TypeError('betterMean : Player in parameter is not a player')
         return self._mean > player2.getMean()
 
     def __str__(self):
+        '''
+        print how the player is
+        :return: None, magical function.
+        '''
         return f'Player:{self.getName()}\nHeight:{self.getHeight()}\nWeight:{self.getWeight()}\nPositions:{self.getPosition()}\nMean:{self.getMean()}'
 
 
