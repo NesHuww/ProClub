efd723099c5e726cbdb689262b7a1136065e6056
Florian Valentin <florian.valentin@etudiant.univ-reims.fr>
2024-06-04 21:02:43 +0200

    Doc Formation.py OK

--- pythonProject/Formation.py
+++ pythonProject/Formation.py
@@ -2,15 +2,18 @@ from Player import *
 
 class Formation:
     def __init__(self, player:list, captain:Player, disposition:str):
+        '''
+        Constructor of Formation class
+        :param player: array of players on the field
+        :param captain: Formation's captain
+        :param disposition: How the team plays : 433/4231/42222
+        '''
 
         if type(disposition) != str:
             raise TypeError('disposition is not a string')
-        #if len(disposition) == 3:
-         #   if disposition[0] + disposition[1] +disposition[2] != 10:
-          #      raise ValueError('there is not 10 players in your team (expected gk)')
-        #else:
-            #if disposition[0] + disposition[1] + disposition[2] + disposition[3] != 10:
-             #   raise ValueError('there are not 10 players in your team (expected gk)')
+        if len(disposition) > 4:
+            raise ValueError('disposition is too long (max: 4)')
+
         self._players = player
         self._captain = captain
         self._disposition = disposition
@@ -18,21 +21,39 @@ class Formation:
 
 
     def getDefense(self)->int:
+        '''
+        Return the number of player playing in defense
+        :return: int
+        '''
         return self._disposition[0]
 
     def getMidfield(self)->int:
+        '''
+        Return the number of player playing in midfield
+        :return: int
+        '''
         return self._disposition[1]
 
     def getAttack(self)->int:
+        '''
+        Return the number of player playing in Attack
+        :return: int
+        '''
         return self._disposition[2]
 
     def getCaptain(self)->Player:
+        '''
+        Return the player who is the captain of the team
+        :return: Player
+        '''
         return self._captain
 
-    def getDisposition(self)->str:
-        return self._disposition
 
     def getStriker(self)-> str:
+        '''
+        Return the number of striker, if there is two steps on midfield
+        :return: int or string if useless
+        '''
         if len(self._disposition) == 4:
             return self._disposition[3]
         else:
@@ -40,6 +61,10 @@ class Formation:
 
 
     def getDisposition(self)->str:
+        '''
+        Return how the team plays on the fied
+        :return: string
+        '''
         content = ""
         if type(self.getStriker()) == str:
             content = f"{self.getDefense()}-{self.getMidfield()}-{self.getAttack()}"
@@ -48,14 +73,23 @@ class Formation:
         return content
 
 
-
-    def addPlayer(self, player):
+    def addPlayer(self, player)->None:
+        '''
+        Add a player on the player list and then add him at the good post
+        :param player: Player you want to have
+        :return: None
+        '''
         if not isinstance(player, Player):
             raise TypeError("addPlayer: player isn't a player")
         self._players.append(player)
         self.assignPost(player)
 
     def removePlayer(self, Player2)->None:
+        '''
+        Remove a player from the player list and then from the team
+        :param Player2:
+        :return:
+        '''
         self.players = [player for player in self.players if player.name != Player2.name]
         if Player in self._team[Player.getPosition()]:
             self._team[Player.getPosition()].remove(Player)
@@ -82,7 +116,7 @@ class Formation:
     def SortTactic(self)->None:
         '''
         Use to sort posts by their position in a field
-        :return:
+        :return: None
         '''
         dic = {}
         for key in Player.posts:
